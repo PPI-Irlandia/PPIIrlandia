d7b6eb8d04f0b8c0dfacac96b63d2b20a1147cbc
ZainulArifin1 <zainula2000nst@gmail.com>
2025-09-13 21:03:38 +0100

    push data portal

--- Data_Students/R_Script.R
+++ Data_Students/R_Script.R
@@ -4,13 +4,15 @@ library(openxlsx)
 library(viridisLite)
 
 
-data <- read.xlsx("PPI_Irlandia/Pengurus_2025/PPIIrlandia/Data_Students/Students_2025_2026_Edition_July2025.xlsx")
+data <- read.xlsx("PPI_Irlandia/Pengurus_2025/PPIIrlandia/Data_Students/")
 data$idx <- seq(1, nrow(data))
 
+
+
+#### Age ####
 data_summary <- data %>%
   count(Age_Range)
 
-# Age
 plot_ly(data = data_summary,
         x = ~Age_Range,
         y = ~n,
@@ -29,7 +31,7 @@ plot_ly(data = data_summary,
          paper_bgcolor = "#ffffff")
 
 
-# Gender
+#### Gender ####
 gender_counts <- data %>%
   count(Gender) %>%
   mutate(
@@ -38,52 +40,116 @@ gender_counts <- data %>%
   )
 
 plot_ly(gender_counts, labels = ~Gender, values = ~n, type = 'pie',
+        hole = 0.4,  # this makes it a ring/donut chart
         text = ~text_label,
         textinfo = 'text',
         insidetextorientation = 'radial',
-        marker = list(colors = c('#636EFA', '#EF553B', '#00CC96', '#AB63FA', '#FFA15A'))) %>%
+        marker = list(colors = c('green', 'blue', '#00CC96', '#AB63FA', '#FFA15A'))) %>%
   layout(title = 'Gender Distribution',
          showlegend = TRUE)
 
-# Cities
-city_counts <- data.frame(table(data$City)) 
-colnames(city_counts) <- c("city", "Freq")
-
-library(plotly)
-library(rjson)
-
-data_map <- fromJSON(file="https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json")
-
-url <- 'https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json'
-counties <- rjson::fromJSON(file=url)
-url2<- "https://raw.githubusercontent.com/plotly/datasets/master/fips-unemp-16.csv"
-df <- read.csv(url2, colClasses=c(fips="character"))
-g <- list(
-  scope = 'usa',
-  projection = list(type = 'albers usa'),
-  showlakes = TRUE,
-  lakecolor = toRGB('white')
-)
-fig <- plot_ly()
-fig <- fig %>% add_trace(
-  type="choropleth",
-  geojson=counties,
-  locations=df$fips,
-  z=df$unemp,
-  colorscale="Viridis",
-  zmin=0,
-  zmax=12,
-  marker=list(line=list(
-    width=0)
+#### Degree ####
+degree_counts <- data %>%
+  count(Degree) %>%
+  mutate(
+    percent = round(100 * n / sum(n), 1),
+    text_label = paste0(Degree, "<br>", percent, "%<br>Count: ", n)
   )
-)
-fig <- fig %>% colorbar(title = "Unemployment Rate (%)")
-fig <- fig %>% layout(
-  title = "2016 US Unemployment by County"
-)
 
-fig <- fig %>% layout(
-  geo = g
-)
+# Define desired order
+desired_order <- c("S1 (Undergraduate)", "S2 (Masters)", "S3 (PhD)")
+degree_counts$Degree <- factor(degree_counts$Degree, levels = desired_order)
+degree_counts$text_label <- factor(degree_counts$text_label, levels = c("S1 (Undergraduate)<br>10.9%<br>Count: 10",
+                                                                    "S2 (Masters)<br>65.2%<br>Count: 60",
+                                                                    "S3 (PhD)<br>23.9%<br>Count: 22"))
+degree_counts <- degree_counts %>% arrange(Degree)
 
-fig
+plot_ly(degree_counts,
+        labels = ~as.character(Degree), 
+        hole = 0.4,
+        values = ~n,
+        type = 'pie',
+        text = ~text_label,
+        textinfo = 'text',
+        insidetextorientation = 'horizontal',
+        marker = list(colors = c('orange', 'blue', "green"))) %>%
+  layout(title = 'Degree Distribution',
+         showlegend = TRUE,
+         legend = list(categoryorder = "array",
+                       categoryarray = desired_order))
+
+#### Cities ####
+city_counts <- data.frame(table(data$City)) 
+colnames(city_counts) <- c("City", "Freq")
+
+library(tmap)
+library(sf)
+
+Sys.setenv("SHAPE_RESTORE_SHX" = "YES")
+ROI_counties = st_read("PPI_Irlandia/Pengurus_2025/PPIIrlandia/Data_Students/26_Counties/26 Counties/Census2011_Admin_Counties_generalised20m.shp")
+ROI_counties <- select(ROI_counties, c(Nation = NUTS1NAME, County = COUNTYNAME, pop = Total2011, ID = COUNTY))
+ROI_counties$City <- gsub(" County", "", ROI_counties$County)
+
+Ireland <- ROI_counties
+Ireland <- left_join(Ireland, city_counts)
+
+tmap_mode("view")
+
+Ireland %>%
+  tm_shape() +
+  tm_fill(col = "Freq",
+          palette = "YlOrRd",          # Yellow to Red palette
+          title = "Distribution",
+          text.size = 0.5, 
+          style = "cont",
+          position = c("top", "left")) +
+  tm_scale_bar(breaks = c(0, 50, 100), text.size = 0.5,
+               position = c("right", "bottom")) +
+  tm_layout(frame = FALSE)
+#### University ####
+data_summary <- data %>%
+  count(University) %>%
+  arrange(n) %>%                    
+  mutate(University = factor(University, levels = unique(University)))
+
+plot_ly(data = data_summary,
+        x = ~n,
+        y = ~University,
+        type = 'bar',
+        orientation = 'h',                # horizontal bars to show universities on y-axis
+        marker = list(
+          color = ~n,
+          colorscale = "Plasma"
+        ),
+        text = ~paste(n),
+        hoverinfo = 'text') %>%
+  layout(title = "Students Distribution in Irish Universities",
+         xaxis = list(title = "Count"),
+         yaxis = list(title = "", categoryorder = "array", categoryarray = levels(data_summary$University)),
+         bargap = 0.2,
+         plot_bgcolor = "#ffffff",
+         paper_bgcolor = "#ffffff")
+
+#### Major ####
+data_summary <- data %>%
+  count(Major_general) %>%
+  arrange(n) %>%                    
+  mutate(Major = factor(Major_general, levels = unique(Major_general)))
+
+plot_ly(data = data_summary,
+        x = ~n,
+        y = ~Major,
+        type = 'bar',
+        orientation = 'h',                # horizontal bars to show universities on y-axis
+        marker = list(
+          color = ~n,
+          colorscale = "Plasma"
+        ),
+        text = ~paste(n),
+        hoverinfo = 'text') %>%
+  layout(title = "Students Majors in Irish Universities",
+         xaxis = list(title = "Count"),
+         yaxis = list(title = "", categoryorder = "array", categoryarray = levels(data_summary$University)),
+         bargap = 0.2,
+         plot_bgcolor = "#ffffff",
+         paper_bgcolor = "#ffffff")
